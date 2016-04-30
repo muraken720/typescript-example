22001497f0e06a1d9763dd6b63fa5b4e25d9781e
muraken720 <kenichiro.murata@gmail.com>
2016-05-01 02:46:00 +0900

    initial commit

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/empower/index.d.ts" />
-/// <reference path="browser/ambient/mocha/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/power-assert/index.d.ts" />
-/// <reference path="browser/ambient/power-assert-formatter/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="main/ambient/empower/index.d.ts" />
-/// <reference path="main/ambient/mocha/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/ambient/power-assert/index.d.ts" />
-/// <reference path="main/ambient/power-assert-formatter/index.d.ts" />
